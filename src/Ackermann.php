87ae85fbf677d232bf15c701dec32a26f0013b94
nicholasnet <nirmalp@hotmail.com>
2016-08-23 15:29:17 -0400

    Ackermann formula refactor.

--- src/Ackermann.php
+++ src/Ackermann.php
@@ -41,7 +41,7 @@ class Ackermann
 
         if ($m < 4) {
 
-            self::applyAckermannFormula($m, $n);
+            self::$resultCache[$m][$n] = self::applyAckermannFormula($m, $n);
 
         } elseif ($n === 0) {
 
@@ -73,33 +73,104 @@ class Ackermann
             return self::$resultCache[$m][$n];
 
         }
+
+        // For non-recursive option if we disregard all other predefined condition and just concentrate on else part of
+        // recursive function we can see that in each iteration we return the same function for $m we decrease value by
+        // 1 and then for $b we pass same function again with $b - 1. So, if we replace function name with some sort of
+        // stack then computation will basically look like this.
+        //
+        // Using recursion computation (only considering the else part of the recursion)
+        // a(4,3)
+        //       a((4 - 1), a(4, (3 - 1))
+        //
+        // As we can see value of m goes down by 1 in first fn call for second fn call value of m remains same but n
+        // goes down by 1
+        //
+        // So, now doing same thing using stack it will be
+        //
+        // a(4,3)
+        //       add (4) into the to be compute stack
+        //
+        //           add (4 - 1) into to be compute stack |
+        //           add (4) into to be compute stack     |-- First iteration
+        //           then decrease 3 by 1                 |
+        //
+        //           add (3 - 1) into to be compute stack |
+        //           add (3) into to be compute stack     |-- Second iteration
+        //           then decrease 2 by 1                 |
+        //
+        // until stack goes empty.
+
+        $toBeSolved = [$m];
+
+        while (!empty($toBeSolved)) {
+
+            $i = array_pop($toBeSolved);
+
+            if ($i < 4) {
+
+                $n = self::applyAckermannFormula($i, $n);
+
+            } elseif ($n === 0) {
+
+                $toBeSolved[] = $i - 1;
+                $n = 1;
+
+            } else {
+
+                $toBeSolved[] = $i - 1; // Decreased value of m
+                $toBeSolved[] = $i;
+                $n = $n - 1; // Decrement n by 1
+            }
+
+            self::$resultCache[$m][$n] = $n;
+        }
+
+        return self::$resultCache[$m][$n];
     }
 
     /**
-     * This method applies Ackermann formula for value of $m which is less than 4.
+     * This will clear the cache(array) of already calculated values.
+     */
+    public static function clearCache()
+    {
+        self::$resultCache = [];
+    }
+
+    /**
+     * This method applies Ackermann formula for value of $m which is less than 4 and returns it.
      *
      * @param int $m
      * @param int $n
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return int
      */
     private function applyAckermannFormula($m, $n)
     {
         switch ($m) {
 
             case 0:
-                self::$resultCache[$m][$n] = $n + 1;
+                $result = $n + 1;
                 break;
 
             case 1:
-                self::$resultCache[$m][$n] = $n + 2;
+                $result = $n + 2;
                 break;
 
             case 2:
-                self::$resultCache[$m][$n] = (2 * $n) + 3;
+                $result = (2 * $n) + 3;
                 break;
 
             case 3:
-                self::$resultCache[$m][$n] = pow(2, ($n + 3)) - 3;
+                $result = pow(2, ($n + 3)) - 3;
                 break;
+
+            default:
+                throw new \InvalidArgumentException('Value of $m must between 0-3');
         }
+
+        return $result;
     }
 }
--- tests/AckermannTest.php
+++ tests/AckermannTest.php
@@ -33,6 +33,7 @@ class AckermannTest extends PHPUnit_Framework_TestCase
      */
     public function computes_value_non_recursively($m, $n, $expectedValues)
     {
+        A::clearCache();
         $this->assertEquals($expectedValues, A::computeNonRecursively($m, $n));
     }
 
@@ -62,6 +63,9 @@ class AckermannTest extends PHPUnit_Framework_TestCase
             [3, 2, 29],
             [3, 3, 61],
             [3, 4, 125],
+            [3, 7, 1021],
+            [4, 0, 13],
+            [4, 1, 65533],
         ];
     }
 }
